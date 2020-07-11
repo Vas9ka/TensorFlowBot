2d4c1a475b2ee4367f632e776dddd493da8bd9c0
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2020-07-11 20:23:50 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import telebot
 import neuralnetwork
+import os
+from flask import Flask, request
 picture_URL = ""
 style_URL = ""
-bot = telebot.TeleBot('1376136022:AAHhUQo_rcGMqI7QeJrzwNsm0hwNt4w4vXc') 
+TOKEN = '1376136022:AAHhUQo_rcGMqI7QeJrzwNsm0hwNt4w4vXc'
+bot = telebot.TeleBot('1376136022:AAHhUQo_rcGMqI7QeJrzwNsm0hwNt4w4vXc')
+server = Flask(__name__) 
 counter = 0
 @bot.message_handler(commands=['start']) 
 def start_message(message):
@@ -34,4 +38,18 @@ def handle_photo(message):
 
 
 
-bot.polling(none_stop= True,interval= 0)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://tensorflowbot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
