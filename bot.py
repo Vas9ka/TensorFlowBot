046cd35461370826daf0bc322003e2ab39d96bc2
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2020-07-12 01:29:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ picture_URL = ""
 style_URL = ""
 TOKEN = '1354173088:AAHaseJVed9YIM4VkFW6znKVA8swPoldpWU'
 bot = telebot.TeleBot('1354173088:AAHaseJVed9YIM4VkFW6znKVA8swPoldpWU')
+user_photos = {}
 server = Flask(__name__) 
-counter = 0
 @bot.message_handler(commands=['start']) 
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет! Этот бот умеет смешивать две картинки так, что первая будет выглядеть в стиле второй.Для дополнительной информации напиши /help')
@@ -20,29 +20,28 @@ def help_message(message):
 
 @bot.message_handler(content_types= ['photo'])
 def handle_photo(message):
-    global counter
-    global style_URL
+    global user_photos
     global picture_URL
-    counter += 1 
-    fileID = message.photo[-1].file_id
-    if(counter == 1):
-        picture_URL = bot.get_file_url(fileID)
-        print(picture_URL)
-    if(counter == 2):
-        style_URL = bot.get_file_url(fileID)
-        print(style_URL)
-        counter = 0
-        neuralnetwork.save_image(picture_URL,style_URL)
-        photo = open('img.png','rb')
-        bot.send_photo(message.chat.id,photo)
-
-
+    global style_URL
+    if not user_photos.get(message.chat.id):
+        user_photos[message.chat.id] = [1]
+        id = message.photo[-1].file_id
+        picture_URL = bot.get_file_url(id)
+        user_photos[message.chat.id].append(picture_URL)
+    else:
+        user_photos[message.chat.id][0] += 1
+    if user_photos[message.chat.id][0] == 2:
+        id = message.photo[-1].file_id
+        style_URL = bot.get_file_url(id)
+        user_photos[message.chat.id].append(style_URL)
+        print(user_photos)
+        bot.send_photo(message.chat.id,neuralnetwork.save_image(user_photos[message.chat.id][1],user_photos[message.chat.id][2]))
+        user_photos[message.chat.id].clear()
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
-
 
 @server.route("/")
 def webhook():
