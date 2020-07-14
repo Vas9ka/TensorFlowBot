58a14806d1f31ccd4ce4172d12f5d40fbe45f883
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2020-07-14 19:58:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://tensorflowbot.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://styletransferbott.herokuapp.com/' + TOKEN)
     return "!", 200
 
 if __name__ == '__main__':
